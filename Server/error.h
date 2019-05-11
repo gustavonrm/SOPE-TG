c8787542158e37708cc8e4a9386b13df47c70bde
NRm <gustavonrm@gmail.com>
2019-05-11 18:21:22 +0100

    fixed file duplicating write

--- Server/error.h
+++ Server/error.h
@@ -2,9 +2,10 @@
 
 #define ARG_ERR 1
 #define FILE_OPEN_ERR 2
-#define FIFO_OPEN_ERR 3
-#define MKFIFO_ERR 4
-#define UNLINK_ERR 5
-#define ACC_CREATE_ERR 6
-#define PIPE_OPEN_ERR 7
-#define FORK_ERR 8
+#define FILE_CLOSE_ERR 3
+#define FIFO_OPEN_ERR 4
+#define MKFIFO_ERR 5
+#define UNLINK_ERR 6
+#define ACC_CREATE_ERR 7
+#define PIPE_OPEN_ERR 8
+#define FORK_ERR 9
--- Server/server.c
+++ Server/server.c
@@ -15,36 +15,40 @@
 
 #include "utils.h"
 
-int  _bank_offices;
-char  _password[MAX_PASSWORD_LEN];
-int  fd;
-int  sFifo;
+int _bank_offices;
+char _password[MAX_PASSWORD_LEN];
+int fd;
+int sFifo;
 bank_account_t admin_account;
 bank_account_t user_account[MAX_BANK_ACCOUNTS];
-sem_t producer, consumer; 
+sem_t producer, consumer;
 
 /////////////////////////////////
 void *PrintHello(void *arg)
 {
-   printf("thread #%ld!\n", pthread_self());
-   pthread_exit(arg);
+  int *x = (int*)arg;
+  printf("thread #%ld!\n", pthread_self());
+  x++;
+  pthread_exit(0);
 }
 /////////////////////////////////
 
+int main(int argc, char *argv[])
+{
 
-int main (int argc, char *argv[]) {
-  
-  if (argc != 3) {
-    fprintf (stderr, "USAGE: %s <bank_offices> <password>\n", argv[0]);
-    exit (ARG_ERR);
+  if (argc != 3)
+  {
+    fprintf(stderr, "USAGE: %s <bank_offices> <password>\n", argv[0]);
+    exit(ARG_ERR);
   }
-  
+
   //slog.txt
-  fd = open (SERVER_LOGFILE, O_WRONLY|O_TRUNC|O_CREAT, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
-  if ( fd == -1) {
-    exit (FILE_OPEN_ERR);
+  fd = open(SERVER_LOGFILE, O_WRONLY | O_TRUNC | O_CREAT, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+  if (fd == -1)
+  {
+    exit(FILE_OPEN_ERR);
   }
-  
+
   //IPC init
   /*
   sem_init(&consumer,0,0);
@@ -53,41 +57,47 @@ int main (int argc, char *argv[]) {
   */
 
   //pass arguments
-  if ((_bank_offices = atoi (argv[1])) > MAX_BANK_OFFICES) {
+  if ((_bank_offices = atoi(argv[1])) > MAX_BANK_OFFICES)
+  {
     _bank_offices = MAX_BANK_OFFICES;
   }
   strcpy(_password, argv[2]);
 
-  pthread_t offices[_bank_offices+1];
-  offices[0]= pthread_self(); 
+  pthread_t offices[_bank_offices + 1];
+  offices[0] = pthread_self();
 
   //#1 create admin acc
-  create_bank_account (&admin_account, ADMIN_ACCOUNT_ID, 0, _password);
-  logAccountCreation (fd, 4, &admin_account);
-
+  create_bank_account(&admin_account, ADMIN_ACCOUNT_ID, 0, _password);
+  logAccountCreation(fd, 4, &admin_account);
   //#2 create electronic banks
-  for (int i=1; i < _bank_offices; i++) {
-    pthread_create(&offices[i],NULL,PrintHello,NULL); //TODO thread func
-    logBankOfficeOpen (fd, i, offices[i]);
-  } 
-
-
-  //#3 create FIFO /tmp/secure_srv
-  if (mkfifo (SERVER_FIFO_PATH, 0660) != 0) {
-    exit (MKFIFO_ERR);
+  for (int i = 1; i <= _bank_offices; i++)
+  {
+    pthread_create(&offices[i], NULL, PrintHello, NULL); //TODO thread func
+    logBankOfficeOpen(fd, i, offices[i]);
   }
 
-  if ((sFifo = open (SERVER_FIFO_PATH, O_RDONLY)) == -1) {
-    exit (FIFO_OPEN_ERR);
+  //#3 create FIFO /tmp/secure_srv
+  if (mkfifo(SERVER_FIFO_PATH, 0660) != 0)
+  {
+    exit(MKFIFO_ERR);
+  }
+
+  if ((sFifo = open(SERVER_FIFO_PATH, O_RDONLY)) == -1)
+  {
+    exit(FIFO_OPEN_ERR);
   }
 
   //fifo echoing, pauses, logs
   //#4 unlink fifo
 
-  //program should only stop when all requests have been processed 
-  if (unlink (SERVER_FIFO_PATH) != 0){
-    exit (UNLINK_ERR);
+  //program should only stop when all requests have been processed
+  if (unlink(SERVER_FIFO_PATH) != 0)
+  {
+    exit(UNLINK_ERR);
   }
+
+  if(close(fd) !=0)
+    exit(FILE_CLOSE_ERR);
 
   return 0;
 }
