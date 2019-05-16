05a32374ad5354e51c5ae5e023640c80d28d8438
NRm <gustavonrm@gmail.com>
2019-05-17 00:21:25 +0100

    tranfer done

--- Server/server.c
+++ Server/server.c
@@ -16,8 +16,8 @@
 #include "operations.h"
 #include "srv_utils.h"
 
-void _cleanUp (int srvFifo, int slogFd);
-void *bank_office_process (void *arg);
+void _cleanUp(int srvFifo, int slogFd);
+void *bank_office_process(void *arg);
 
 ////////GLOBAL/////////
 int slogFd;
@@ -37,115 +37,122 @@ sync_role_t crole = SYNC_ROLE_CONSUMER;
 sync_role_t prole = SYNC_ROLE_PRODUCER;
 sync_role_t arole = SYNC_ROLE_ACCOUNT;
 
-int main (int argc, char *argv[]) {
-  if (argc != 3) {
-    fprintf (stderr, "USAGE: %s <bank_offices> <password>\n", argv[0]);
-    exit (ARG_ERR);
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
+    fprintf(stderr, "USAGE: %s <bank_offices> <password>\n", argv[0]);
+    exit(ARG_ERR);
   }
 
-  slogFd = open (SERVER_LOGFILE, O_WRONLY | O_TRUNC | O_CREAT, S_IRWUSR | S_IRGRP | S_IROTH);
+  slogFd = open(SERVER_LOGFILE, O_WRONLY | O_TRUNC | O_CREAT, S_IRWUSR | S_IRGRP | S_IROTH);
   if (slogFd == -1)
-    exit (FILE_OPEN_ERR);
+    exit(FILE_OPEN_ERR);
 
-  int numOffices = atoi (argv[1]);
+  int numOffices = atoi(argv[1]);
   if (numOffices > MAX_BANK_OFFICES)
     return INVALID_INPUT_ERR;
 
   pthread_t offices[numOffices];
-  offices[0] = pthread_self ();
+  offices[0] = pthread_self();
 
   // IPC init
   // TODO CHANGE WHEN IN IPC IMPLEMENTATION
-  logSyncMechSem (slogFd, 0, smo, prole, getpid (), 0);
-  sem_init (&full, 0, 0);
-  logSyncMechSem (slogFd, 0, smo, prole, getpid (), numOffices);
-  sem_init (&empty, 0, numOffices);
+  logSyncMechSem(slogFd, 0, smo, prole, getpid(), 0);
+  sem_init(&full, 0, 0);
+  logSyncMechSem(slogFd, 0, smo, prole, getpid(), numOffices);
+  sem_init(&empty, 0, numOffices);
 
-  if (create_bank_account (&admin_account, ADMIN_ACCOUNT_ID, 0, argv[2]) != 0)
+  if (create_bank_account(&admin_account, ADMIN_ACCOUNT_ID, 0, argv[2]) != 0)
     return ACC_CREATE_ERR;
   accounts[0] = admin_account;
-  logAccountCreation (slogFd, 0000, &admin_account);
-  
-  for (int i = 1; i <= numOffices; i++) {
-    pthread_create (&offices[i], NULL, bank_office_process, (void *)&i);
-    logBankOfficeOpen (slogFd, i, offices[i]);
+  logAccountCreation(slogFd, 0000, &admin_account);
+
+  for (int i = 1; i <= numOffices; i++)
+  {
+    pthread_create(&offices[i], NULL, bank_office_process, (void *)&i);
+    logBankOfficeOpen(slogFd, i, offices[i]);
   }
 
-  if (mkfifo (SERVER_FIFO_PATH, S_IRWUSR | S_IRWGRP) != 0)
-    exit (MKFIFO_ERR);
+  if (mkfifo(SERVER_FIFO_PATH, S_IRWUSR | S_IRWGRP) != 0)
+    exit(MKFIFO_ERR);
 
-  int srvFifo = open (SERVER_FIFO_PATH, O_RDONLY);
+  int srvFifo = open(SERVER_FIFO_PATH, O_RDONLY);
   if (srvFifo == -1)
-    exit (FIFO_OPEN_ERR);
+    exit(FIFO_OPEN_ERR);
 
   int nBytes;
-  while (1) {
+  while (1)
+  {
     tlv_request_t request;
 
-    nBytes = read (srvFifo, &request, sizeof (op_type_t) + sizeof (uint32_t));
+    nBytes = read(srvFifo, &request, sizeof(op_type_t) + sizeof(uint32_t));
     if (nBytes == -1)
-      exit (FIFO_READ_ERR);
+      exit(FIFO_READ_ERR);
 
     if (nBytes == 0)
       continue;
 
-    nBytes = read (srvFifo, &request.value, request.length);
+    nBytes = read(srvFifo, &request.value, request.length);
     if (nBytes == -1)
-      exit (FIFO_READ_ERR);
+      exit(FIFO_READ_ERR);
 
     if (nBytes == 0)
       continue;
 
-    print_request (request);
+    print_request(request);
     //log
     smo = SYNC_OP_COND_WAIT;
-    sem_getvalue (&empty, &sem_val);
-    logSyncMechSem (slogFd, 0, smo, prole, getpid (), sem_val);
+    sem_getvalue(&empty, &sem_val);
+    logSyncMechSem(slogFd, 0, smo, prole, getpid(), sem_val);
     //ipc
-    sem_wait (&empty);
+    sem_wait(&empty);
     //log
     smo = SYNC_OP_MUTEX_LOCK;
-    logSyncMech (slogFd, 0, smo, prole, getpid ());
+    logSyncMech(slogFd, 0, smo, prole, getpid());
     //ipc
-    pthread_mutex_lock (&mut);
+    pthread_mutex_lock(&mut);
     //critical region
-    queuePush (request);
+    queuePush(request);
     //ipc
-    pthread_mutex_unlock (&mut);
+    pthread_mutex_unlock(&mut);
     //log
     smo = SYNC_OP_MUTEX_UNLOCK;
-    logSyncMech (slogFd, 0, smo, prole, getpid ());
+    logSyncMech(slogFd, 0, smo, prole, getpid());
     //ipc
-    sem_post (&full);
+    sem_post(&full);
     //log
     smo = SYNC_OP_SEM_POST;
-    sem_getvalue (&full, &sem_val);
-    logSyncMechSem (slogFd, 0, smo, prole, getpid (), sem_val);
+    sem_getvalue(&full, &sem_val);
+    logSyncMechSem(slogFd, 0, smo, prole, getpid(), sem_val);
   }
 
-  _cleanUp (srvFifo, slogFd);
+  _cleanUp(srvFifo, slogFd);
 
   return 0;
 }
 
-void _cleanUp (int srvFifo, int slogFd) {
-  if (close (srvFifo))
-    exit (FIFO_CLOSE_ERR);
+void _cleanUp(int srvFifo, int slogFd)
+{
+  if (close(srvFifo))
+    exit(FIFO_CLOSE_ERR);
 
-  if (unlink (SERVER_FIFO_PATH) != 0)
-    exit (UNLINK_ERR);
+  if (unlink(SERVER_FIFO_PATH) != 0)
+    exit(UNLINK_ERR);
 
-  if (close (slogFd) != 0)
-    exit (FILE_CLOSE_ERR);
+  if (close(slogFd) != 0)
+    exit(FILE_CLOSE_ERR);
 
-  queueDelete ();
+  queueDelete();
 }
 
 //////////////////THREADS///////////////////////
-void *bank_office_process (void *arg) {
-  printf ("thread #%ld!\n", pthread_self ());
+void *bank_office_process(void *arg)
+{
+  printf("thread #%ld!\n", pthread_self());
   int index = (*(int *)arg);
-  while (1) {
+  while (1)
+  {
     tlv_request_t request;
     tlv_reply_t reply;
     //ret_code_t ret;
@@ -153,57 +160,63 @@ void *bank_office_process (void *arg) {
 
     //log
     smo = SYNC_OP_SEM_WAIT;
-    sem_getvalue (&full, &sem_val);
-    logSyncMechSem (slogFd, index, smo, crole, getpid (), sem_val);
+    sem_getvalue(&full, &sem_val);
+    logSyncMechSem(slogFd, index, smo, crole, getpid(), sem_val);
     //1# IPC
-    sem_wait (&full);
+    sem_wait(&full);
     //log
     smo = SYNC_OP_MUTEX_LOCK;
-    logSyncMech (slogFd, index, smo, prole, getpid ());
+    logSyncMech(slogFd, index, smo, prole, getpid());
     //ipc
-    pthread_mutex_lock (&mut);
+    pthread_mutex_lock(&mut);
     //2#receive -- CRITICAL REGION
-    request = queuePop ();
+    request = queuePop();
     //apos acessar conta!!!! atraso
-    delay (request);
-    logDelay (slogFd, index, request.value.header.op_delay_ms); //logSyncDelay()???
+    delay(request);
+    logDelay(slogFd, index, request.value.header.op_delay_ms); //logSyncDelay()???
     //fifo name
-    sprintf (USER_FIFO_PATH, "%s%d", USER_FIFO_PATH_PREFIX, request.value.header.pid);
+    sprintf(USER_FIFO_PATH, "%s%d", USER_FIFO_PATH_PREFIX, request.value.header.pid);
 
-    switch (request.type) {
+    switch (request.type)
+    {
     case OP_CREATE_ACCOUNT:
     {
       ret_code_t ret;
       uint32_t id = request.value.create.account_id;
-      
-      ret = checkLogin (&(accounts[0]), request.value.header.password);
-      if (ret != RC_OK){
-        reply = makeErrorReply (&request, RC_LOGIN_FAIL);
-        writeToFifo (reply, USER_FIFO_PATH);
+
+      ret = checkLogin(&(accounts[0]), request.value.header.password);
+      if (ret != RC_OK)
+      {
+        reply = makeErrorReply(&request, RC_LOGIN_FAIL);
+        writeToFifo(reply, USER_FIFO_PATH);
         break;
       }
 
-      if (request.value.header.account_id != ADMIN_ACCOUNT_ID) {
-        reply = makeErrorReply (&request, RC_OP_NALLOW);
-        writeToFifo (reply, USER_FIFO_PATH);
+      if (request.value.header.account_id != ADMIN_ACCOUNT_ID)
+      {
+        reply = makeErrorReply(&request, RC_OP_NALLOW);
+        writeToFifo(reply, USER_FIFO_PATH);
         break;
       }
-      if (accounts[id].account_id == id) {
-        reply = makeErrorReply (&request, RC_ID_IN_USE);
-        writeToFifo (reply, USER_FIFO_PATH);
+
+      if (accounts[id].account_id == id)
+      {
+        reply = makeErrorReply(&request, RC_ID_IN_USE);
+        writeToFifo(reply, USER_FIFO_PATH);
         break;
       }
-      
-      ret = create_bank_account (&accounts[id], request.value.create.account_id, request.value.create.balance, request.value.create.password);
-      if (ret == RC_OTHER) {
-        reply = makeErrorReply (&request, RC_OTHER);
-        writeToFifo (reply, USER_FIFO_PATH);
+
+      ret = create_bank_account(&accounts[id], request.value.create.account_id, request.value.create.balance, request.value.create.password);
+      if (ret == RC_OTHER)
+      {
+        reply = makeErrorReply(&request, RC_OTHER);
+        writeToFifo(reply, USER_FIFO_PATH);
         break;
       }
-      reply = makeReply (&request, accounts[id].balance);
-      
-      logAccountCreation (slogFd, request.value.create.account_id, &accounts[id]);
-      writeToFifo (reply, USER_FIFO_PATH);
+      reply = makeReply(&request, accounts[id].balance);
+
+      logAccountCreation(slogFd, request.value.create.account_id, &accounts[id]);
+      writeToFifo(reply, USER_FIFO_PATH);
       break;
     }
 
@@ -211,25 +224,71 @@ void *bank_office_process (void *arg) {
     {
       ret_code_t ret;
       uint32_t id = request.value.header.account_id;
-      
-      ret = checkLogin (&(accounts[id]), request.value.header.password);
-      if (ret != RC_OK) {
+
+      if(accounts[id].account_id == 0){
+        reply = makeErrorReply(&request, RC_ID_NOT_FOUND);
+        writeToFifo(reply, USER_FIFO_PATH);
+        break;
+      }
+
+      ret = checkLogin(&(accounts[id]), request.value.header.password);
+      if (ret != RC_OK)
+      {
         reply = makeErrorReply(&request, ret);
-        writeToFifo (reply, USER_FIFO_PATH);
+        writeToFifo(reply, USER_FIFO_PATH);
         break;
       }
-      if (request.value.header.account_id == 0) {
-        reply = makeErrorReply (&request, RC_OP_NALLOW);
-        writeToFifo (reply, USER_FIFO_PATH);
+      if (request.value.header.account_id == 0)
+      {
+        reply = makeErrorReply(&request, RC_OP_NALLOW);
+        writeToFifo(reply, USER_FIFO_PATH);
         break;
       }
-      reply = makeReply (&request, accounts[id].balance);
-      writeToFifo (reply, USER_FIFO_PATH);
+      reply = makeReply(&request, accounts[id].balance);
+      writeToFifo(reply, USER_FIFO_PATH);
       break;
     }
 
     case OP_TRANSFER:
+    {
+      ret_code_t ret;
+      uint32_t src_id = request.value.header.account_id;
+      uint32_t dest_id = request.value.transfer.account_id;
+
+      if(accounts[src_id].account_id == 0){
+        reply = makeErrorReply(&request, RC_ID_NOT_FOUND);
+        reply.value.transfer.balance=0;
+        writeToFifo(reply, USER_FIFO_PATH);
+        break;
+      }
+
+      ret = checkLogin(&(accounts[src_id]), request.value.header.password);
+      if (ret != RC_OK)
+      {
+        reply = makeErrorReply(&request, RC_LOGIN_FAIL);
+        writeToFifo(reply, USER_FIFO_PATH);
+        break;
+      }
+
+      if (request.value.header.account_id == ADMIN_ACCOUNT_ID)
+      {
+        reply = makeErrorReply(&request, RC_OP_NALLOW);
+        writeToFifo(reply, USER_FIFO_PATH);
+        break;
+      }
+
+      ret = transfer_between_accounts(&accounts[src_id],&accounts[dest_id],request.value.transfer.amount);
+      if(ret != RC_OK)
+      {
+        reply = makeErrorReply(&request, ret);
+        writeToFifo(reply, USER_FIFO_PATH);
+        break;
+      }
+      
+      reply = makeReply(&request, accounts[src_id].balance);
+      writeToFifo(reply, USER_FIFO_PATH);
       break;
+    }
 
     case OP_SHUTDOWN:
       if (request.value.header.account_id != ADMIN_ACCOUNT_ID)
@@ -242,17 +301,17 @@ void *bank_office_process (void *arg) {
     }
 
     //3# IPC
-    pthread_mutex_unlock (&mut);
+    pthread_mutex_unlock(&mut);
     //log
     smo = SYNC_OP_MUTEX_UNLOCK;
-    logSyncMech (slogFd, index, smo, crole, getpid ());
+    logSyncMech(slogFd, index, smo, crole, getpid());
     //IPC
-    sem_post (&empty);
+    sem_post(&empty);
     //log
     smo = SYNC_OP_SEM_POST;
-    sem_getvalue (&empty, &sem_val);
-    logSyncMechSem (slogFd, index, smo, crole, getpid (), sem_val);
+    sem_getvalue(&empty, &sem_val);
+    logSyncMechSem(slogFd, index, smo, crole, getpid(), sem_val);
   }
 
-  printf ("thread #%ld!\n", pthread_self ());
+  printf("thread #%ld!\n", pthread_self());
 }
